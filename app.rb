bdd53704fe6a9f1b9042136f9a448aec6f6bf231
camclark02 <164952709+camclark02@users.noreply.github.com>
2024-04-09 19:40:05 +0000

    finished /rock string

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
 
 get("/") do
   "
@@ -7,3 +8,37 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+    moves = ["rock", "paper", "scissors"]
+    
+    comp_move = moves.sample
+  
+    if comp_move == "rock"
+      outcome = "tied"
+    elsif comp_move == "paper"
+      outcome = "lost"
+    else
+      outcome = "won"
+    end
+  
+    "
+    <h2>
+      We played rock!
+    </h2>
+  
+    <h2>
+     They played #{comp_move}!
+    </h2>
+  
+    <h2>
+      We #{outcome}!
+    </h2>
+    "
+  
+end
+  
+  
+  
+  
+  
